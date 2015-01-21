b5ba53e01b4e4f3de86f9c6abb522dddf787cf85
Michael Brash <mbrash123@gmail.com>
2015-01-21 13:33:59 -0500

    Day-7 javascript buttons, listeners and objects

---
+++ Day-7/js/main.js
@@ -0,0 +1,41 @@
+var myNames = ["mike", "patrick", "chris", "alex"]
+
+var car = {
+	doors: 4,
+	honk: function() {
+		console.log("Honk Honk")
+	}
+}
+
+// listener for button 1
+document.getElementById("1").addEventListener('click', function() {
+	console.log(myNames[1])
+});
+
+// listener for button 2
+document.getElementById("2").addEventListener('click', function() {
+	for (var i = 1; i <= 10; i++) {
+		console.log(i);
+	}
+});
+
+// listener for button 3
+document.getElementById("3").addEventListener('click', function() {
+	for (var i=0; i < myNames.length; i++) {
+		console.log(myNames[i]);
+	}
+})
+
+// listener for button 4
+document.getElementById("4").addEventListener('click', function() {
+	for (var i=0; i < myNames.length; i++) {
+			if (myNames[i].substring(0,1) === 'p') {
+				console.log(myNames[i]);
+			}
+	}
+})
+
+// listener for button 5
+document.getElementById("5").addEventListener('click', function() {
+	console.log(car)
+})
