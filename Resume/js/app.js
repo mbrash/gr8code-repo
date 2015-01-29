fafcbc31bbd6e9860d6b29bf7aff8a05f45291b5
Michael Brash <mbrash123@gmail.com>
2015-01-29 10:33:09 -0500

    fixed resume issues

--- Resume/js/app.js
+++ Resume/js/app.js
@@ -1,4 +1,4 @@
-'user strict'
+'use strict';
 
 $(document).ready(function() {
 
@@ -20,11 +20,19 @@ $(document).ready(function() {
         stopOnHover: true
     });
 
-    // number counter
-    $('.timer').countTo();
-
     // wow js
     new WOW().init();
+
+    // trigger count to on scroll
+    var eventCalled = false
+
+    $(window).scroll(function (event) {
+    	var scroll = $(window).scrollTop();
+    	if (scroll > 560 && !eventCalled) {
+    		eventCalled = true;
+    		$('.timer').countTo();
+    	}
+	});
 
     // ---------------------------------------------------------------------------
     // HANDLE FORM VALIDATION, FORM HINTS, FORM SUBMIT 
@@ -32,74 +40,70 @@ $(document).ready(function() {
 
     // validate name
     function checkName(name) {
-        if (name.length > 0) {
+        if (name.length > 1) {
             return name;
         } else {
             $('#name').focus();
-            $label = '<label class="wow shake">Please Enter Valid Name</label>'; // create html element
-            $('form').append($label);
+            $('form').append('<label class="wow shake">Please Enter Valid Name</label>');
             return false;
-        };
+        }
     };
 
     // validate email
     function checkEmail(email) {
-        var testresults;
         var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
         if (filter.test(email)) {
             return email;
         } else {
             $('#email').focus(); // set focus to field
-            $label = '<label class="wow shake">Please Enter Valid E-mail</label>'; // create html element
-            $('form').append($label);
+            $('form').append('<label class="wow shake">Please Enter Valid E-mail</label>');
             return false;
-        };
+        }
     };
 
     // validate text field 
     function checkTextField(textfield) {
-        if (textfield.length > 0) {
+    	// var filter =
+        if (textfield.length > 3) {
             return textfield;
         } else {
             $('#textField').focus();
-            $label = '<label class="wow shake">Please Enter Valid Message</label>'; // create html element
-            $('form').append($label);
+            $('form').append('<label class="wow shake">Please Enter Valid Message</label>');
             return false;
-        };
+        }
     };
 
     // listen for submit button pressed 
     $('form').on('submit', function(event) {
-        event.preventDefault();
-
-        // removal all labels
-        $('form label').remove();
+        console.log("dsd")
+        event.preventDefault(); // stop form changin page
+        $('form label').remove(); // removal all labels
 
         // get form values
         var nameVal = $('#name').val(),
             emailVal = $('#email').val(),
             textFieldVal = $('#textField').val();
 
-        // submit 
-        if (checkName(nameVal) && checkEmail(emailVal) && checkTextField(textFieldVal)) {
-            // submit form
-            $.ajax({
-                type: "POST",
-                url: $('form').attr('action'), // some php file
-                data: {
-                    "name": nameVal,
-                    "email": emailVal,
-                    "message": textFieldVal
-                },
-                dataType: 'json',
-                success: function() {
-                    console.log("post was succsesful");
-                }
-            });
+        // if form data valid
+        if (checkName(nameVal) && 
+        	checkEmail(emailVal) && 
+        	checkTextField(textFieldVal)) {
+            
+            // get form action
+            var url = $(this).attr("action"),
+            	data = {
+            			 "nameVal" : nameVal,
+            			 "emailVal": emailVal,
+            			 "textFieldVal": textFieldVal
+            			}
 
-            // alert user, and remove form
-            $label = '<label class="wow fadeIn">Messaged Sent!</label>'; // create html element
-            $('form').append($label);
+            // post form
+            $.post(url, data, function(){
+				console.log("posted")
+			})
+
+            // show post received 
+            $('form').append('<label class="wow fadeIn">Messaged Sent!</label>');
 
             // clear text from each input
             $('#name').val("");
